58f61e0b5ce038434f9748a9984750378c1609ff
Florian Aumeier <flo.aumeier@gmail.com>
2016-11-16 16:48:45 +0100

    created types Field, Board. Implemented createBoard. Added .gitignore

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,25 @@
 module Lib
     ( someFunc
+    , Field(..)
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+-- Data Types
+
+data Field = Field 
+                    { x :: Int
+                    , y :: Int
+                    , hasMine :: Bool
+                    } deriving (Show)
+
+data Board = Board [Field] [Field] deriving (Show) 
+
+-- Methods
+
+createFields :: Int -> [Field]
+createFields m = [ Field x y False | x <- [1..m], y <- [1..m]]
+
+printRow :: Board -> Int -> [Field]
+printRow board i = putStrLn row where row =  
