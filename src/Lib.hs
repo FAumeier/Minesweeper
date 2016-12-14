21211b157186805cf879a93679be5eec62b2a480
Florian Aumeier <flo.aumeier@gmail.com>
2016-12-14 14:20:48 +0100

    correct implementation of showBoard

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,13 @@
 module Lib
     ( someFunc
     , Field(..)
+    , Board(..)
+    , createBoard
+    , showBoard
+    , stringBoard
     ) where
+
+import Data.List
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
@@ -11,15 +17,31 @@ someFunc = putStrLn "someFunc"
 data Field = Field 
                     { x :: Int
                     , y :: Int
-                    , hasMine :: Bool
+                    , hasMine :: Bool -- Noch ein Feld -> Opened :: Bool
                     } deriving (Show)
 
-data Board = Board [Field] [Field] deriving (Show) 
+data Board = Board
+                   { fields :: [Field]
+                   , size :: Int
+                   } deriving (Show) 
 
 -- Methods
 
-createFields :: Int -> [Field]
-createFields m = [ Field x y False | x <- [1..m], y <- [1..m]]
+createBoard :: Int -> Board
+createBoard m = Board [ Field x y False | x <- [1..m], y <- [1..m]] m
 
-printRow :: Board -> Int -> [Field]
-printRow board i = putStrLn row where row =  
+--printRow :: Board -> [Field] -- Testweise irgendeine Reihe
+
+-- convert to printable row
+--makePrintableField :: Field -> (String, String)
+--makePrintableField x y = (xAsString, yAsString) where xAsString = show x
+--                                                      yAsString = show y
+
+stringBoard :: Board -> [String]
+stringBoard board = [ if y feld >= size board
+                      then show (x feld, y feld, hasMine feld) ++ "\n"
+                      else show (x feld, y feld, hasMine feld) | feld <- fields board
+                    ]
+
+showBoard :: [String] -> IO ()
+showBoard strings = putStrLn board where board = intercalate " " strings
