e5980d35f82a2aee1600f7d3de65af6f0ab15afb
Florian Aumeier <flo.aumeier@gmail.com>
2016-12-16 14:50:56 +0100

    added updateBoard Method to update a certain field

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,12 @@
 module Lib
     ( Field(..)
     , Board(..)
-    , (!!!)
     ) where
 
 import Data.List
 
 data Field = Marked | Unmarked | Open Int | Mine
-  deriving (Show)
+  deriving (Show, Eq)
 
 type Board = [[Field]]
 
@@ -22,3 +21,16 @@ data State = Won | Lost | Undecided
 
 (!!!) :: Board -> Coordinates -> Field
 (!!!) [fields] (x, y) = [fields] !! x !! y
+
+updateBoard :: Board -> Coordinates -> Field -> Board
+updateBoard [fields] (x, y) field = take x [fields] ++ [updatedRow] ++ drop (x+1) [fields]
+            where row = [fields] !! x
+                  updatedRow = replaceField row y field
+
+replaceField :: [Field] -> Int -> Field -> [Field]
+replaceField fields i element = take i fields ++ [element] ++ drop (i+1) fields
+
+  --- Kopiere alle Reihen bis Reihe x
+  --- Kopiere alle Elemente bis Element y
+  --- Füge das neue Feld an Stelle von Element y ein
+  --- Kopiere den Rest
