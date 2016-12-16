89674f79bd30c81cc605b885dcb715ed7b2d036b
Florian Aumeier <flo.aumeier@gmail.com>
2016-12-16 12:59:43 +0100

    Redone. Defined new Types and Datatypes. Added (\!\!\!) Method

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,61 +1,24 @@
 module Lib
-    ( someFunc
-    , Field(..)
+    ( Field(..)
     , Board(..)
-    , createBoard
-    , showBoard
-    , stringBoard
-    , openField
-    , setFieldOpen
+    , (!!!)
     ) where
 
 import Data.List
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+data Field = Marked | Unmarked | Open Int | Mine
+  deriving (Show)
 
--- Data Types
+type Board = [[Field]]
 
-data Field = Field
-                    { xCor :: Int
-                    , yCor :: Int
-                    , hasMine :: Bool -- Noch ein Feld -> Opened :: Bool
-                    , surroundingMines :: Int
-                    , isOpened :: Bool
-                    } deriving (Show)
+data GameState = GameState { board :: Board
+                           , mines :: [Coordinates]
+                           , state :: State
+                           }
+type Coordinates = (Int, Int)
+data State = Won | Lost | Undecided
 
-data Board = Board
-                   { fields :: [Field]
-                   , size :: Int
-                   } deriving (Show)
+-- data Action = Toggle Coordinates | openField Coordinates
 
--- Methods
-
-createBoard :: Int -> Board
-createBoard m = Board [ Field x y False 0 False | x <- [1..m], y <- [1..m]] m
-
---printRow :: Board -> [Field] -- Testweise irgendeine Reihe
-
--- convert to printable row
---makePrintableField :: Field -> (String, String)
---makePrintableField x y = (xAsString, yAsString) where xAsString = show x
---                                                      yAsString = show y
-
-stringBoard :: Board -> [String]
-stringBoard board = [ if yCor feld >= size board
-                      then show (xCor feld, yCor feld, hasMine feld, surroundingMines feld, isOpened feld) ++ "\n"
-                      else show (xCor feld, yCor feld, hasMine feld, surroundingMines feld, isOpened feld) | feld <- fields board
-                    ]
-
-showBoard :: [String] -> IO ()
-showBoard strings = putStrLn board where board = intercalate " " strings
-
-openField :: Int -> Int -> Board -> [Field]
-openField x y (Board fields m) = [ if xCor field == x && yCor field == y
-                                   then setFieldOpen field
-                                   else field | field <- fields]
-setFieldOpen :: Field -> Field
-setFieldOpen oldField = Field x y mine surr True where x = xCor oldField
-                                                       y = yCor oldField
-                                                       mine = hasMine oldField
-                                                       surr = surroundingMines oldField
+(!!!) :: Board -> Coordinates -> Field
+(!!!) [fields] (x, y) = [fields] !! x !! y
