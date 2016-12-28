a763ec9c2a6020ab12ccc89e2d485a0bab70887f
Florian Aumeier <flo.aumeier@gmail.com>
2016-12-28 13:11:06 +0100

    refactoring in Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -33,16 +33,16 @@ generateCoordinates size =
         in randomizeCoordinates 15 liste []
 
 randomizeCoordinates :: Int -> [(Int, Int)] -> [(Int, Int)] -> [Coordinates]
-randomizeCoordinates 0 _ ys = ys
-randomizeCoordinates i (x:xs) ys
-  | x `elem` ys = randomizeCoordinates i xs ys
-  | otherwise = randomizeCoordinates (i-1) xs (x:ys)
+randomizeCoordinates 0 _ newCoors = newCoors
+randomizeCoordinates i (coor:coors) newCoors
+  | coor `elem` newCoors = randomizeCoordinates i coors newCoors
+  | otherwise = randomizeCoordinates (i-1) coors (coor:newCoors)
 
 main =
   do
       let size = 8 -- size of the board
       let gamestate = start size
-      play (InWindow "Minesweeper 0.1" (wsizex,wsizey) (10,10)) white 1 gamestate toPicture handleEvent (\float world -> world)
+      play (InWindow "Minesweeper 0.1" (wsizex,wsizey) (10,10)) white 1 gamestate getPicture handleEvent (\float world -> world)
 
 -- handleEvent handles mouse events
 handleEvent (EventKey (MouseButton button) Down _ (x,y)) gs =
@@ -62,39 +62,51 @@ handleEvent (EventKey (Char 'q') Down _ _) gs = error "Stop!"
 handleEvent event state = state
 
 -- generate picture for the gamestate
-toPicture :: GameState -> Picture
-toPicture gs = Pictures $ field:message
+getPicture :: GameState -> Picture
+getPicture gs = Pictures $ field:message
   where
+    -- calculate size of board from gamestate
+   xlen  = fromIntegral $ length $ (board gs) !! 0
+   ylen  = fromIntegral $ length $ (board gs)
    -- the board
-   field =  Translate ((-1)*fwsizex/2) (fwsizey/2) $  Scale scale scale $ Pictures [Translate (j-1) (-1*i) $ toCell e | (i,zs) <- toTup board , (j,e) <- zs]
+   field = getField xlen ylen gs
    -- a message if won or lost
    message = case state gs of
                  Lost -> [Translate ((-1)*fwsizex/2) ((-1)*fwsizey/4) $ Color (light $ light red) (Scale (fwsizex) (fwsizey/2)  (Polygon [(0,0),(1,0),(1,1),(0,1),(0,0)]))
-                         ,Translate ((-1)*fwsizex/2.1) ((-1)*fwsizey/16) $ Text "You lost!"]
+                         ,Translate ((-1)*fwsizex/2.1) ((-1)*fwsizey/16) $ Text "Verloren!"]
                  Won  -> [Translate ((-1)*fwsizex/2) ((-1)*fwsizey/4) $ Color (light $ light green) (Scale (fwsizex) (fwsizey/2)  (Polygon [(0,0),(1,0),(1,1),(0,1),(0,0)]))
-                         ,Translate ((-1)*fwsizex/2.1) ((-1)*fwsizey/16) $ Text "You won!"]
+                         ,Translate ((-1)*fwsizex/2.1) ((-1)*fwsizey/16) $ Text "Gewonnen!"]
                  other -> []
-   -- Board to coordinates
-   toTup matrix = zip [1..] (map (zip [1..]) (board gs))
-   -- calculate size of board from gamestate
-   xlen  = fromIntegral $ length $ (board gs)!!0
-   ylen  = fromIntegral $ length $ (board gs)
-   -- scaling of a field
-   scale = min (fwsizex/xlen) (fwsizey/ylen)
-   -- picture of a field
-   toCell Marked   = flag
-   toCell Unmarked = hidden
-   toCell (Open i) = num i
-   toCell Mine     = bomb
-   -- Mine
-   bomb = Pictures [box red [(0,0),(1,0),(1,1),(0,1),(0,0)],Translate (0.5) (0.5) $ Color black (circleSolid 0.3)]
-   -- Marks
-   flag = Pictures [box (greyN 0.7)  [(0,0),(1,0),(1,1),(0,1),(0,0)],
-                    (Pictures [box (light $ yellow) [(0.7,0.1),(0.7,0.85),(0.85,0.85),(0.85,0.1),(0.7,0.1)],
-                               box (dark $ red)     [(0.2,0.6),(0.8,0.6),(0.8,0.8),(0.2,0.8),(0.2,0.6)]])]
-   -- hidden
-   hidden = box  (greyN 0.5) [(0,0),(1,0),(1,1),(0,1),(0,0)]
-   -- field open, display number of mines
-   num i  = Pictures [box yellow [(0,0),(1,0),(1,1),(0,1),(0,0)],Translate 0.2 (0.1) $ Scale 0.007 0.007 (Text (show i))]
-   -- generate colorful boxes
-   box col xs = Pictures [Color col (Polygon xs), Color black (Line xs)]
+
+
+getField xlen ylen gs = Translate ((-1)*fwsizex/2) (fwsizey/2) $  Scale (scaleField xlen ylen) (scaleField xlen ylen) $ Pictures [Translate (j-1) (-1*i) $ toCell e | (i,zs) <- boardToCoordinates board gs, (j,e) <- zs]
+
+boardToCoordinates bd gs = zip [1..] (map (zip [1..]) (board gs))
+
+scaleField xlen ylen = min (fwsizex/xlen) (fwsizey/ylen)
+
+toCell :: Field -> Picture
+toCell field = case field of
+  Marked   -> getFlag
+  Unmarked -> getHiddenbox
+  (Open i) -> minesInProximity i
+  Mine     -> getBomb
+--  otherwise -> getHiddenbox
+
+
+getBomb = Pictures [box red [(0,0),(1,0),(1,1),(0,1),(0,0)],Translate (0.5) (0.5) $ Color black (circleSolid 0.3)]
+
+getFlag = Pictures [box (greyN 0.7)  [(0,0),(1,0),(1,1),(0,1),(0,0)],
+                 (Pictures [box (light $ yellow) [(0.7,0.1),(0.7,0.85),(0.85,0.85),(0.85,0.1),(0.7,0.1)],
+                            box (dark $ red)     [(0.2,0.6),(0.8,0.6),(0.8,0.8),(0.2,0.8),(0.2,0.6)]])]
+
+getHiddenbox = box (greyN 0.5) [(0,0),(1,0),(1,1),(0,1),(0,0)]
+
+minesInProximity i  =
+  if i > 2 then Pictures [box orange [(0,0),(1,0),(1,1),(0,1),(0,0)],Translate 0.2 (0.1) $ Scale 0.007 0.007 (Text (show i))]
+  else if i > 0
+    then Pictures [box yellow [(0,0),(1,0),(1,1),(0,1),(0,0)],Translate 0.2 (0.1) $ Scale 0.007 0.007 (Text (show i))]
+  else Pictures [box green [(0,0),(1,0),(1,1),(0,1),(0,0)],Translate 0.2 (0.1) $ Scale 0.007 0.007 (Text (show i))]
+
+-- generate colorful boxes
+box col xs = Pictures [Color col (Polygon xs), Color black (Line xs)]
